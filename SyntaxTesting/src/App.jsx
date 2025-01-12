98784d2b6219342f89503d1b8846f7df109de1b2
shranav-1148 <shranavsakha78@gmail.com>
2025-01-12 11:02:14 -0600

    Adding some Details

--- SyntaxTesting/src/App.jsx
+++ SyntaxTesting/src/App.jsx
@@ -7,7 +7,31 @@ import { useState } from 'react';
  * Created for Syntax Practice and Understanding 
  * @returns MyButton object
  */
-function MyButton(){
+function MyButton({ count, onClick }){
+  /**
+   * If we want to count separately uncomment this and remove the arguments
+   */
+
+
+  // const [count, setCount] = useState(0);
+
+  // function handleClick(){
+  //   setCount(count+1);
+  // }
+
+  return (
+    <button onClick={onClick}>
+      Clicked {count} times
+    </button>
+  )
+}
+
+function App() {
+  /**
+   * This is for when count is updated on both buttons regardless of which we click
+   * If for counting separately, comment the useState, handleClick code out and in the MyButton
+   * HTML tags remove count and onClick, since we won't need to pass them from the App
+   */
   const [count, setCount] = useState(0);
 
   function handleClick(){
@@ -15,20 +39,11 @@ function MyButton(){
   }
 
   return (
-    <button onClick={handleClick}>
-      Clicked {count} times
-    </button>
-  )
-}
-
-function App() {
-  
-
-  return (
     <div>
       <h1>Counters Using UseState</h1>
-      <MyButton />
-      <MyButton />
+      <MyButton count = {count} onClick={handleClick} />
+      <MyButton count = {count} onClick={handleClick} />
+      
 
     </div>
   );
